180270541a8dd4d5602c887c45f7b18920b7badf
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2024-05-05 00:41:16 +0530

    Create sketch_may4a-lightBlink.ino

---
+++ sketch_may4a-lightBlink/sketch_may4a-lightBlink.ino
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  int pinSet[] = {12,27,25,32,13};
+  for (int i = 0; i < (sizeof(pinSet)/sizeof(pinSet[0])); i++){
+    pinMode(pinSet[i],OUTPUT);
+  }
+
+}
+
+void loop() {
+  int pinSet[] = {12,27,25,32,13};
+  int arrLen = (sizeof(pinSet)/sizeof(pinSet[0]));
+  // put your main code here, to run repeatedly:
+   for (int i = 0; i < arrLen ; i++){
+     digitalWrite(pinSet[i],HIGH);
+     delay(500);
+      digitalWrite(pinSet[arrLen-i],HIGH);
+      delay(500);
+     }
+for (int i = 0; i < arrLen ; i++){
+     digitalWrite(pinSet[i],LOW);
+     delay(500);
+      digitalWrite(pinSet[arrLen-i],LOW);
+      delay(500);
+     }
+
+     //digitalWrite(pinSet)
+   }
+  
+
+
